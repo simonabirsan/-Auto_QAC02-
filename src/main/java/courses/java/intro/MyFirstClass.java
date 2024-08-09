170dbe98cc7c6b93bd6154323522d7752a743b5a
DESKTOP-OBJ24SN\Simona <simona.hirean@gmail.com>
2024-08-09 13:09:43 +0300

    add new new java class

--- src/main/java/courses/java/intro/MyFirstClass.java
+++ src/main/java/courses/java/intro/MyFirstClass.java
@@ -4,5 +4,17 @@ public class MyFirstClass {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!" );
+  //    define variable
+        int x;
+
+        x=10;
+        System.out.println(x);
+        boolean isPrime=true;
+
+        char ch='A';
+//      definim o constanta si adaugam final la double
+        final double PI=3.14;
     }
 }
+
+
