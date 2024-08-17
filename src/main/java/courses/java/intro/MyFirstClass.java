b0506bc7253a9fbceba62904921042c125221d89
DESKTOP-OBJ24SN\Simona <simona.hirean@gmail.com>
2024-08-17 13:02:42 +0300

    add new new java class

--- src/main/java/courses/java/intro/MyFirstClass.java
+++ src/main/java/courses/java/intro/MyFirstClass.java
@@ -1,5 +1,7 @@
 package courses.java.intro;
 
+import java.sql.SQLOutput;
+
 public class MyFirstClass {
 
     public static void main(String[] args) {
@@ -14,6 +16,24 @@ public class MyFirstClass {
         char ch='A';
 //      definim o constanta si adaugam final la double
         final double PI=3.14;
+        System.out.println(PI*12*12);
+        String firstName = "Mr . Popescu Ion";
+        firstName = "Vasile P";
+
+
+        System.out.println("division:" + ((float)5 / 2));
+        System.out.println("Modulo:" + 5 % 2);
+ //       compound assignemnt
+        x = x + 1;
+        System.out.println(x);
+        x*=2;
+        System.out.println(x);
+ //       post increment
+
+
+
+
+
     }
 }
 
